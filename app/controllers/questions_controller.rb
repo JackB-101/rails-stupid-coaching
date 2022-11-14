4e7d62b33c6c7991830007800708941d8b19e03b
Jack Burton <burtonjackpg@gmail.com>
2022-11-14 16:12:37 +0000

    update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @questions = params[:question]
+    if @questions == "hello"
+      @answers = "get to work"
+    elsif params[:question].end_with?("?")
+      @answers = "silly questions get to work"
+    else
+      @answers = "stop being lazy"
+    end
   end
 end
